25dc997098b7ae46d0c5515a9b22c79dc26f6035
PranishShresth <yunche64@gmail.com>
2022-07-18 11:50:34 +1000

    task: fix retro timer and add useInterval hook

--- components/RetroBoard/RetroTimer.tsx
+++ components/RetroBoard/RetroTimer.tsx
@@ -1,188 +1,113 @@
-import { Box, Button, Flex, useInterval } from "@chakra-ui/react";
-import {
-  collection,
-  doc,
-  getDocs,
-  onSnapshot,
-  orderBy,
-  query,
-  serverTimestamp,
-  updateDoc,
-  where,
-} from "firebase/firestore";
+import { Badge, Box, Button, Flex, IconButton } from "@chakra-ui/react";
+import { useInterval } from "hooks/useInterval";
+
 import { firestore } from "configs/firebase/firestore";
-import { differenceInMilliseconds } from "date-fns";
-import { format, zonedTimeToUtc } from "date-fns-tz";
-import { useEffect, useRef, useState } from "react";
+import { zonedTimeToUtc } from "date-fns-tz";
+import { useState } from "react";
 import { Colours } from "../ColourPicker";
 import { Board } from "utils/interfaces";
 import { FIVE_MINUTES_IN_SECONDS } from "components/RetroHome/RetroBody";
-import { useAuthContext } from "context/Auth/AuthContext";
-
+import { doc, updateDoc, serverTimestamp } from "firebase/firestore";
+import { AiOutlineClockCircle } from "react-icons/ai";
+import { BsStopCircle } from "react-icons/bs";
 interface RetroTimerProps {
   board: Board;
 }
+function getTimeRemaining(endtime: string) {
+  const currentTime = zonedTimeToUtc(
+    new Date(),
+    Intl.DateTimeFormat().resolvedOptions().timeZone
+  ).toUTCString();
 
+  const total = Date.parse(endtime) - Date.parse(currentTime);
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
+}
 const padTo2Digits = (num: number) => {
   return num.toString().padStart(2, "0");
 };
 
 export const RetroTimer = ({ board }: RetroTimerProps) => {
-  const { userDetails } = useAuthContext();
-  const [isTimerStarted, setIsTimerStarted] = useState(false);
   const [timeLeft, setTimeLeft] = useState<string | null>(null);
-  const intervalRef = useRef<any>();
+  const hasTimerStarted = !!board?.timer?.startAt;
 
-  // console.log("board", board);
+  console.log(hasTimerStarted);
 
-  // const snapshot = onSnapshot(
-  //   doc(firestore, "boards", board.board_id),
-  //   (doc) => {
-  //     console.log("Current data: ", doc.data());
-  //   }
-  // );
-
-  useEffect(() => {
-    if (userDetails) {
-      const q = query(
-        collection(firestore, "boards"),
-        where("createdBy.user_id", "==", userDetails.user_id)
-      );
-
-      const boardsCollectionSnap = onSnapshot(q, (snapshot) => {
-        console.log("snapshot", snapshot.docs);
-        const payload = snapshot.docs.map((doc) => {
-          console.log(doc.data()?.timer?.startAt);
-          const startAt = doc.data()?.timer?.startAt;
-          const seconds = FIVE_MINUTES_IN_SECONDS;
-          // const startAt = snapshot.val().startAt;
-          const interval = setInterval(() => {
-            const timeLeft = seconds * 1000 - (Date.now() - startAt);
-            if (timeLeft < 0) {
-              clearInterval(interval);
-              console.log("0.0 left");
-            } else {
-              console.log(`${Math.floor(timeLeft / 1000)}.${timeLeft % 1000}`);
-            }
-          }, 1000);
-          // return { ...doc.data(), doc_id: doc.id } as BoardWithDocId;
-        });
-        // setBoards(payload);
-        // setLoading(false);
-      });
-      return boardsCollectionSnap;
-    }
-  }, [userDetails]);
-
-  // if (board.timer?.startAt) {
-  //   const endsAt = board.timer.startAt.seconds + FIVE_MINUTES_IN_SECONDS * 1000;
-
-  //   const countDown = () => {
-  //     setTime(Math.max(0, endsAt - Date.now()));
-  //     console.log("timeLeft", timeLeft);
-  //     if (endsAt <= Date.now()) {
-  //       console.log("finsihed");
-  //       clearInterval(intervalRef.current);
-  //     }
-  //   };
-
-  //   const setTime = (remaining: number) => {
-  //     console.log("remaining", remaining);
-  //     var minutes = Math.floor(remaining / 60000);
-  //     var seconds = Math.round(remaining / 1000);
-  //     setTimeLeft(`${padTo2Digits(minutes)}:${padTo2Digits(seconds)}`);
-  //   };
-
-  //   intervalRef.current = setInterval(countDown, 1000);
-  // }
-
-  // useEffect(() => {
-  //   intervalRef.current = setInterval(() => {
-  //     if (board.timer?.startAt) {
-  //       const a = format(
-  //         new Date(board.timer.startAt.seconds * 1000),
-  //         "hh:mm:ss"
-  //       );
-
-  //       const result = differenceInMilliseconds(
-  //         zonedTimeToUtc(
-  //           new Date(),
-  //           Intl.DateTimeFormat().resolvedOptions().timeZone
-  //         ),
-  //         new Date(board.timer.startAt.seconds * 1000)
-  //       );
-  //       console.log("result", result / 1000);
-  //       const differenceInSeconds = result / 1000;
-
-  //       if (differenceInSeconds >= 10) {
-  //         console.log("time is up");
-  //         clearInterval(intervalRef.current);
-  //       }
-
-  //       const minutes = Math.floor(differenceInSeconds / 60);
-
-  //       // 👇️ get remainder of seconds
-  //       const seconds = Math.trunc(differenceInSeconds % 60);
-
-  //       setTimeLeft(`${padTo2Digits(minutes)}:${padTo2Digits(seconds)}`);
-
-  //       return () => {
-  //         clearInterval(intervalRef.current);
-  //       };
-  //     }
-  //     console.log("setinterval running");
-  //   }, 1000);
-  // }, [board.timer?.startAt]);
-
+  const icon = hasTimerStarted ? <BsStopCircle /> : <AiOutlineClockCircle />;
   const handleStartTimer = async () => {
+    const ref = doc(firestore, "boards", board.board_id);
     try {
-      const ref = doc(firestore, "boards", board.board_id);
-
       await updateDoc(ref, {
         timer: {
           startAt: serverTimestamp(),
         },
       });
-
-      setIsTimerStarted(true);
+      clear();
     } catch (err) {
       console.log(err);
     }
   };
 
   const handleResetTimer = async () => {
-    try {
-      const ref = doc(firestore, "boards", board.board_id);
+    const ref = doc(firestore, "boards", board.board_id);
 
+    try {
       await updateDoc(ref, {
         timer: {
           startAt: null,
         },
       });
-
-      setIsTimerStarted(false);
+      setTimeLeft("");
     } catch (err) {
       console.log(err);
     }
   };
 
+  const clear = useInterval(
+    () => {
+      if (board.timer?.startAt) {
+        const ref = doc(firestore, "boards", board.board_id);
+
+        const endTime = new Date(
+          board.timer.startAt.seconds * 1000 + FIVE_MINUTES_IN_SECONDS * 1000
+        ).toUTCString();
+        const { minutes, seconds, total } = getTimeRemaining(endTime);
+
+        if (total <= 0) {
+          clear();
+          updateDoc(ref, {
+            timer: {
+              startAt: null,
+            },
+          });
+        }
+        setTimeLeft(`${padTo2Digits(minutes)}:${padTo2Digits(seconds)}`);
+      }
+    },
+    1000,
+    [board.timer?.startAt]
+  );
+  const onClick = hasTimerStarted ? handleResetTimer : handleStartTimer;
+
   return (
     <Flex alignItems={"center"} justifyContent="space-between">
-      {isTimerStarted && timeLeft && (
-        <Box marginRight={"1rem"}>Retro starts in: {timeLeft}</Box>
+      {hasTimerStarted && (
+        <Box marginRight={"1rem"}>
+          <Badge colorScheme="red">{timeLeft}</Badge>
+        </Box>
       )}
       <Box>
-        {isTimerStarted ? (
-          <Button
-            color={Colours.fireOpal}
-            onClick={handleResetTimer}
-            variant={"outline"}
-          >
-            Reset Timer
-          </Button>
-        ) : (
-          <Button onClick={handleStartTimer}>Start Timer</Button>
-        )}
+        <IconButton onClick={onClick} aria-label={"timer"} icon={icon} />
       </Box>
     </Flex>
   );
