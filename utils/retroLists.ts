c3f04e5a087e59b20276ae5b0dcbc32b2f173eee
Benjamin Liu <benliu1070@hotmail.com>
2022-08-21 14:56:50 +1000

    feat: add retro board list template for australia team

---
+++ utils/retroLists.ts
@@ -0,0 +1,8 @@
+export const AUSTRALIA_RETRO_LISTS = [
+  { listColour: "#28a745", listTitle: "Went well" },
+  { listColour: "#fbbd08", listTitle: "Updates" },
+  { listColour: "#e8575b", listTitle: "Needs improving" },
+  { listColour: "#007bff", listTitle: "Actions" },
+  { listColour: "#a333c8", listTitle: "Behaviour" },
+  { listColour: "#f0de77", listTitle: "Shoutouts" },
+];
