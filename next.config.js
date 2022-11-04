b7bf729d4ab16d3667eb6fb26745729225909fdd
PranishShresth <yunche64@gmail.com>
2022-11-05 00:26:51 +1100

    feat: configure pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   async rewrites() {
     return [{ source: "/", destination: "/index.html" }];
@@ -9,4 +10,14 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  disable: process.env.NODE_ENV === "development",
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
+});
+
+module.exports = withPWA(nextConfig);
