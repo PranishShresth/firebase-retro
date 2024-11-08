c9b917ae57e7d46f0673f3f307238a57c475e769
Pranish Shrestha <shresthapranish99@gmail.com>
2024-11-09 00:44:19 +1100

    modify text

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,16 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import { AuthProvider } from "context/Auth/AuthContext";
-import { ColorModeScript } from "@chakra-ui/react";
-import theme from "./../theme";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import "@fontsource/inter/400.css";
 import "@fontsource/open-sans/700.css";
 import ErrorBoundary from "components/ErrorBoundary";
+import { AuthProvider } from "context/Auth/AuthContext";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import theme from "./../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <ErrorBoundary fallback={<p>Something went wrong!</p>}>
+      <ErrorBoundary fallback={<p>Something went wrong! Please try again</p>}>
         <AuthProvider>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Component {...pageProps} />
